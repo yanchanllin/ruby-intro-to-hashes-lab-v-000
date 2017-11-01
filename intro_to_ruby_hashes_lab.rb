0ecb225a27e483a2c92c8f671c97d98afd70f585
YanChan  Lin <yonalin1@gmail.com>
2017-11-01 03:39:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_second_tier
     },
     :names=>
     {
-      
+
       },
-      
+
   }
 }
 
@@ -52,19 +52,19 @@ def monopoly_with_third_tier
     {
       :reading_railroad=>
       {
-        
+
       },
       :pennsylvania_railroad=>
       {
-        
+
       },
       :b_and_o_railroad=>
       {
-        
+
       },
       :shortline=>
       {
-        
+
       }
     }
   }
